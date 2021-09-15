d8ceb8423d4cd0d5e82d64a90fa3b63fd90a89a2
algmoreno <alg.moreno00@gmail.com>
2021-09-15 14:40:06 -0700

    client folder

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas')
 const db = require('./config/connection');
-const { authMiddleware } = require('./utils/auth')
+const { authMiddleware } = require('./utils/auth');
+const path = require('path')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,6 +20,14 @@ async function startServer() {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.statu(path.join(__dirname, '../client/build')))
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
